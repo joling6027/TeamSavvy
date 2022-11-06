ab678793c900c34361de5e208b37631b18f21456
kaurjasmine09 <kaurj153@student.douglascollege.ca>
2022-11-05 17:48:51 -0700

    Create EmployeeDetails.jsx

---
+++ User Interface/teamsavvy/src/components/employeeDetails/EmployeeDetails.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {Container, Form, FormGroup, Label, Input, FormFeedback, FormText, Row, Col, Modal,  ModalHeader, Button, ModalBody } from 'reactstrap';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Timesheet from '../timesheet/Timesheet'
+import Task from '../task/Task'
+import Profile from '../profile/Profile'
+
+const EmployeeDetails = () => {
+    return ( 
+        <>
+        <Container className="px-3">
+            <p className="h4 px-2 pb-3">Employee Name</p>
+            
+            <Tabs
+            defaultActiveKey="home"
+            
+            transition={false}
+            id="noanim-tab-example"
+            className="mb-3"
+            >
+            <Tab eventKey="home" title="Timesheet">
+                <Timesheet/>
+            </Tab>
+            <Tab eventKey="profile" title="Task">
+                {/* <Task/> */}
+            </Tab>
+            <Tab eventKey="contact" title="Profile" >
+                {/* <Profile/> */}
+            </Tab>
+            </Tabs>
+
+        </Container>
+        </>
+    );
+}
+
+export default EmployeeDetails;
