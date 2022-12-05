43c5a0d3c041876572e8a420d1e7effbcd574c98
Jaspal Singh <jaspal3101@gmail.com>
2022-12-04 19:53:58 -0800

    MOBILE APP

--- Biometric Mobile App/AuthMyEmployee/app/src/main/java/com/example/authmyemployee/MainActivity.java
+++ Biometric Mobile App/AuthMyEmployee/app/src/main/java/com/example/authmyemployee/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 import androidx.biometric.BiometricPrompt;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
@@ -32,12 +33,16 @@ import org.json.JSONObject;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnAuthentication;
+    Button btnClose;
     TextView empAuthStatus;
     TextView empId;
 
@@ -47,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RequestQueue requestQueue;
     Boolean res;
+    boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,24 +60,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnAuthentication = findViewById(R.id.btnAuth);
+        btnAuthentication.setText("AUTHENTICATE EMPLOYEE");
         btnAuthentication.setEnabled(false);
+        btnClose = findViewById(R.id.btnClose);
+        findViewById(R.id.emp_id).setEnabled(true);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+                findViewById(R.id.emp_id).setEnabled(true);
+            }
+        });
 
         empAuthStatus = findViewById(R.id.empAuthStatus);
         empId = findViewById(R.id.emp_id);
 
         requestQueue = VolleySingleton.getInstance(this).getRequestQueue();
-
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            res = extras.getBoolean("flag",false);
-            if(res){
-                btnAuthentication.setText("CLOCK OUT");
-            }
-            else{
-                btnAuthentication.setText("CLOCK IN");
-            }
-        }
-
 
         empId.addTextChangedListener(new TextWatcher() {
             @Override
@@ -85,12 +90,12 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                     btnAuthentication.setEnabled(false);
+                    btnAuthentication.setText("AUTHENTICATE EMPLOYEE");
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
             }
         });
 
@@ -104,22 +109,33 @@ public class MainActivity extends AppCompatActivity {
                 empId.setText("");
             }
 
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onAuthenticationSucceeded(@NonNull androidx.biometric.BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
-                if(res){
+                if(btnAuthentication.getText().toString().equals("AUTHENTICATE EMPLOYEE"))
+                {
+                    GetEmployeeData(empId.getText().toString());
+                }
+                if(btnAuthentication.getText().toString().equals("CLOCK OUT")){
                     ClockOut();
+                    empId.setText("");
+                    btnAuthentication.setText("AUTHENTICATE EMPLOYEE");
+                    findViewById(R.id.emp_id).setEnabled(true);
                 }
-                else{
+                if(btnAuthentication.getText().toString().equals("CLOCK IN")){
                     ClockIn();
+                    empId.setText("");
+                    btnAuthentication.setText("AUTHENTICATE EMPLOYEE");
+                    findViewById(R.id.emp_id).setEnabled(true);
                 }
-                empId.setText("");
+
             }
 
             @Override
             public void onAuthenticationFailed() {
                 super.onAuthenticationFailed();
-                empAuthStatus.setText("Employee Not Verified");
+                empAuthStatus.setText("Finger print is not from trusted source");
             }
         });
 
@@ -134,16 +150,24 @@ public class MainActivity extends AppCompatActivity {
         biometricPrompt.authenticate(promptInfo);
     }
 
+    /*
+     * Send Clock In time via API to Team Savvy Database
+     * */
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void ClockIn(){
         String url = getResources().getString(R.string.ClockIn);
         JSONObject reqObj = new JSONObject();
         try {
-            reqObj.put("employeeId",Integer.parseInt(empId.getText().toString()));
-            reqObj.put("firstName","Jaspal");
-            reqObj.put("lastName","Singh");
-            reqObj.put("password","value");
-            reqObj.put("token","value");
-            reqObj.put("role","value");
+            LocalDate dateObj = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String date = dateObj.format(formatter);
+            LocalTime localTime = LocalTime.now();
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
+            String time = localTime.format(dateTimeFormatter);
+            reqObj.put("employeeId", Integer.parseInt(empId.getText().toString()));
+            reqObj.put("clockDate", date);
+            reqObj.put("clockTime",time);
+            reqObj.put("clockType","Clock-In");
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -153,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            if(response.getBoolean("data")){
+                            if(response.getBoolean("success")){
                                 Snackbar.make((findViewById(android.R.id.content)),"Employee Verified!!\nClock In", Snackbar.LENGTH_LONG)
                                         .setActionTextColor(getResources().getColor(android.R.color.black ))
                                         .setBackgroundTint(getResources().getColor(R.color.purple_500))
@@ -182,16 +206,21 @@ public class MainActivity extends AppCompatActivity {
     /*
     * Send Clock out time via API to Team Savvy Database
     * */
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void ClockOut(){
         String url = getResources().getString(R.string.ClockOut);
         JSONObject reqObj = new JSONObject();
         try {
+            LocalDate dateObj = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String date = dateObj.format(formatter);
+            LocalTime localTime = LocalTime.now();
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
+            String time = localTime.format(dateTimeFormatter);
             reqObj.put("employeeId", Integer.parseInt(empId.getText().toString()));
-            reqObj.put("firstName","Jaspal");
-            reqObj.put("lastName","Singh");
-            reqObj.put("password","value");
-            reqObj.put("token","value");
-            reqObj.put("role","value");
+            reqObj.put("clockDate",date);
+            reqObj.put("clockTime",time);
+            reqObj.put("clockType","Clock-Out");
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -201,7 +230,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            if(response.getBoolean("data")){
+                            if(response.getBoolean("success")){
                                 Snackbar.make((findViewById(android.R.id.content)),"Employee Verified!!\nClock Out", Snackbar.LENGTH_LONG)
                                         .setActionTextColor(getResources().getColor(android.R.color.black ))
                                         .setBackgroundTint(getResources().getColor(R.color.purple_500))
@@ -215,6 +244,53 @@ public class MainActivity extends AppCompatActivity {
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void GetEmployeeData(String empId) {
+        String url = getResources().getString(R.string.GetData) + '/' + empId;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if(response.getBoolean("success") == true)
+                            {
+                                if(response.getString("response").equals("ClockOut"))
+                                {
+                                    btnAuthentication.setText("CLOCK OUT");
+                                }
+                                else
+                                {
+                                    btnAuthentication.setText("CLOCK IN");
+                                }
+
+                                findViewById(R.id.emp_id).setEnabled(false);
+                            }
+
+                            if(response.getBoolean("success") == false)
+                            {
+                                Snackbar.make((findViewById(android.R.id.content)),"Employee not exist!!", Snackbar.LENGTH_LONG)
+                                        .setActionTextColor(getResources().getColor(android.R.color.black ))
+                                        .setBackgroundTint(getResources().getColor(R.color.purple_500))
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Snackbar.make((findViewById(android.R.id.content)),"Employee not exist!!", Snackbar.LENGTH_LONG)
+                                    .setActionTextColor(getResources().getColor(android.R.color.black ))
+                                    .setBackgroundTint(getResources().getColor(R.color.purple_500))
+                                    .show();
                         }
                     }
                 },
--- Biometric Mobile App/AuthMyEmployee/app/src/main/java/com/example/authmyemployee/StartupActivity.java
+++ Biometric Mobile App/AuthMyEmployee/app/src/main/java/com/example/authmyemployee/StartupActivity.java
@@ -20,50 +20,20 @@ import java.util.TimerTask;
 
 public class StartupActivity extends AppCompatActivity {
 
-    private RequestQueue requestQueue;
-    boolean flag = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
-        requestQueue = VolleySingleton.getInstance(this).getRequestQueue();
         Intent intent = new Intent(StartupActivity.this, MainActivity.class);
-        GetEmployeeData(intent);
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
                 startActivity(intent);
             }
         };
-
         Timer timer = new Timer();
-        timer.schedule(timerTask, 5000);
+        timer.schedule(timerTask, 3000);
     }
 
-    private void GetEmployeeData(Intent intent) {
-        String url = getResources().getString(R.string.GetData);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                              if(!response.getString("employeeId").isEmpty())
-                              {
-                                  flag = true;
-                              }
-                             intent.putExtra("flag", flag);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(StartupActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
-        requestQueue.add(jsonObjectRequest);
-    }
+
 }
